65e32a93a7804d4af89ca1a972aeaa86a02209bc
Chesney Daal <c.daal@icloud.com>
2018-02-09 12:12:37 +0100

    v1.0.2 fix the form and faster the process

--- form.js
+++ form.js
@@ -1,10 +1,10 @@
 "use strict";
+var formInput = document.forms["formulier"];
 function validate(){
-	document.getElementsByTagName("input")[2].value = 'Hoi';
+	formInput["submit"].value = 'Hoi';
 }
 function getData(){
-	var formInput = document.getElementsByTagName("input");
 	document.getElementsByTagName("body")[0].innerHTML =
-		 formInput[0].value  + " " +
-		 formInput[1].value; 
+		formInput["name"].value  + " " +
+		formInput["surname"].value; 
 }
